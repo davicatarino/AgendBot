59bd4565f52fe324c36ee35f299a37607d23215d
davicatarino <davi.catarino@hotmail.com>
2024-09-21 12:56:12 -0300

    ignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from './src/app.js';
-const PORT = process.env.PORT || 57420;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
