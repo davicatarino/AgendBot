ef941e822a2524006b69a0899ec8fe72574c4f6c
davicatarino <davi.catarino@hotmail.com>
2024-09-21 17:00:15 -0300

    event function

--- src/services/google/eventFunctions.js
+++ src/services/google/eventFunctions.js
@@ -1,5 +1,5 @@
 // services/google/eventFunctions.js
-import { handleEvent } from './handleEvent.js';
+import { handleEvent } from './handleEvent';
 import { handleAvailable } from './HandleAvailable.js';
 import { handleDelete } from './handleDelete.js';
 
